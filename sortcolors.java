8da5cfbf1b32670e09dfec7924f8a91d88177a4b
kevindhir <kevindhir@alumni.ubc.ca>
2017-11-18 15:49:17 -0800

    Create sortcolors.java

---
+++ sortcolors.java
@@ -0,0 +1,41 @@
+https://leetcode.com/problems/sort-colors/description/
+
+class Solution {
+    public void sortColors(int[] nums) {
+        if(nums.length == 0){
+            return;
+        }
+        int numRed = 0;
+        int numWhite = 0;
+        int numBlue = 0;
+        for(int i = 0; i < nums.length; i++){
+            switch(nums[i]){
+                case 0: 
+                    numRed++;
+                    break;
+                case 1:
+                    numWhite++;
+                    break;
+                case 2: 
+                    numBlue++;
+                    break;
+            }
+        }
+        int j = 0;
+        while(numRed > 0){
+            nums[j] = 0;
+            numRed--;
+            j++;
+        }
+        while(numWhite > 0){
+            nums[j] = 1;
+            numWhite--;
+            j++;
+        }
+        while(numBlue > 0){
+            nums[j] = 2;
+            numBlue--;
+            j++;
+        }
+    }
+}
