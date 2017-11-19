9ef40f02b75c570a773c69923ee02b027480548d
kevindhir <kevindhir@alumni.ubc.ca>
2017-11-19 15:38:13 -0800

    Update reversesinglylinked.java

--- reversesinglylinked.java
+++ reversesinglylinked.java
@@ -21,3 +21,29 @@ class Solution {
         return prev;
     }
 }
+
+
+//Recursively
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        return reverseListHelper(head, null);
+    }
+    
+    private static ListNode reverseListHelper(ListNode current, ListNode prev){
+        if(current == null){
+            return prev;
+        }
+        ListNode tmp = current.next;
+        current.next = prev;
+        return reverseListHelper(tmp, current);
+    }
+    
+}
